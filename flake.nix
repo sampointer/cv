4a353c9edf55e42c2a8d681deebf787f33234e8c
Sam Pointer <sam@outsidethe.net>
2022-08-10 13:11:44 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Sam's CV";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+
+    {
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+          pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.pandoc
+            ];
+       });
+    };
+}
